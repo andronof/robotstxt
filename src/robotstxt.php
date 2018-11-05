cc5a0e6ac11028824b7843caa8b146682a74486d
andronof <andronof@mail.ru>
2018-11-05 17:33:06 +0300

    Поиск соответствий переписан
    Используется регулярные выражения. Добавлены тесты.

--- src/robotstxt.php
+++ src/robotstxt.php
@@ -1,79 +1,91 @@
 <?php
-class Robotstxt 
+
+class Robotstxt
 {
-    var $rules = array();
-    var $current_user_agent = null;
-    function __construct($text = '') 
+    private $rules = array();
+    private $current_user_agent = null;
+
+    public function __construct($text = '')
     {
         if ($text != '') {
             $this->init($text);
         }
     }
-    
-    function setUserAgent($user_agent = null)
+
+    public function setUserAgent($user_agent = null)
     {
         $this->current_user_agent = $user_agent;
     }
-   
-    function init($text) 
+
+    private function preg_quote_robotstxt($url)
     {
-       $text = explode("\n", $text);
-       $user_agent = "";
-       $this->rules = array();
-       foreach($text as $t) {
-           $t = explode("#", $t);
-           $t = trim($t[0]);
-           $params = explode(':', $t);
-           if ( count($params) !== 2) {
+        $str = preg_quote($url, "#");
+        $str = str_replace(array("\\*", "\\$"), array("*", "$"), $str);
+        return $str;
+    }
+
+    public function init($text)
+    {
+        $text = explode("\n", $text);
+        $user_agent = "";
+        $this->rules = array();
+        foreach ($text as $t) {
+            $t = explode("#", $t);
+            $t = trim($t[0]);
+            $params = explode(':', $t);
+            if (count($params) !== 2) {
                 continue;
-           }
-           $params[0] = mb_strtolower(trim($params[0]), "utf-8");
-           $params[1] = mb_strtolower(trim($params[1]), "utf-8");
-           
-           if ( $params[0] == 'user-agent') {
-               $user_agent = $params[1];
-               continue;
-           } elseif ($params[0] == 'allow' || $params[0] == 'disallow') {
+            }
+            $params[0] = mb_strtolower(trim($params[0]), "utf-8");
+            $params[1] = trim($params[1]);
+
+            if ($params[0] == 'user-agent') {
+                $user_agent = mb_strtolower($params[1], "UTF-8");
+                $this->rules[$user_agent] = array();
+                continue;
+            } elseif ($params[0] == 'allow' || $params[0] == 'disallow') {
                 if ($user_agent == '') {
                     continue;
                 }
-                if (mb_substr($params[1], -1 , 1, "UTF-8") == '$' ||
-                    mb_substr($params[1], -1 , 1, "UTF-8") == '*') {
-                        $value = $params[1];
-                } else {
-                    $value = $params[1].'*';
-                }
-                // Если пустые параметры
-                if ($params[1] == "") {
+                if ($params[1] == "") { // Если пустой параметр
                     if ($params[0] === 'disallow') {
                         $params[0] = 'allow';
                     } else {
-                        $params[0] = 'disallow';
+                        continue;
+                    }
+                    $pattern = "#^(.*)$#";
+                } else {
+                    $value = str_replace("*", "(.*)", $this->preg_quote_robotstxt($params[1]));
+                    if (mb_substr($value, 0, 1, "UTF-8") === "/") {
+                        $pattern = "#^" . $value;
+                    } else {
+                        $pattern = "#^(.*)" . $value;
+                    }
+
+                    if (mb_substr($value, -1, 1, "UTF-8") === "$") {
+                        $pattern .= "#";
+                    } else {
+                        $pattern .= "(.*)$#";
                     }
                 }
 
-                if (mb_substr($value, 0, 2, "UTF-8") == '/*') {
-                    $value = substr($value, 2);
-                }
-                
-                
                 $this->rules[$user_agent][] = array(
-                                        'name' => $params[0],
-                                        'value' => $value,
-                                        'evalue' => explode('*', $value)
-                                    );
-           }
+                    'name' => $params[0],
+                    'value' => $params[1],
+                    'pattern' => $pattern
+                );
+
+            }
         }
-       
-        
-        
-        foreach($this->rules as $key=>$user_agent) {
-                uasort($user_agent, array($this, 'cmp'));
-                $this->rules[$key] = $user_agent;
+
+        foreach ($this->rules as $key => $user_agent) {
+            uasort($user_agent, array($this, 'cmp'));
+            $this->rules[$key] = $user_agent;
         }
         return true;
     }
-    function cmp($a, $b)
+
+    private function cmp($a, $b)
     {
         if (mb_strlen($a['value'], "UTF-8") == mb_strlen($b['value'], "UTF-8")) {
             if ($a['name'] == 'allow') {
@@ -85,78 +97,54 @@ class Robotstxt
         }
         return (mb_strlen($a['value'], "UTF-8") < mb_strlen($b['value'], "UTF-8")) ? -1 : 1;
     }
-    function isUserAgent($user_agent)
+
+    public function isUserAgent($user_agent)
     {
         return isset($this->rules[$user_agent]);
     }
-    function unparse_url($parsed_url)
-    { 
-        $scheme   = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : ''; 
-        $host     = isset($parsed_url['host']) ? $parsed_url['host'] : ''; 
-        $port     = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : ''; 
-        $user     = isset($parsed_url['user']) ? $parsed_url['user'] : ''; 
-        $pass     = isset($parsed_url['pass']) ? ':' . $parsed_url['pass']  : ''; 
-        $pass     = ($user || $pass) ? "$pass@" : ''; 
-        $path     = isset($parsed_url['path']) ? $parsed_url['path'] : ''; 
-        $query    = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : ''; 
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : ''; 
-        return "$scheme$user$pass$host$port$path$query$fragment"; 
-    } 
-   
-   function isAllowed($url, $user_agent="*")
-   {
+
+    private function unparse_url($parsed_url)
+    {
+        $scheme = isset($parsed_url['scheme']) ? $parsed_url['scheme'] . '://' : '';
+        $host = isset($parsed_url['host']) ? $parsed_url['host'] : '';
+        $port = isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '';
+        $user = isset($parsed_url['user']) ? $parsed_url['user'] : '';
+        $pass = isset($parsed_url['pass']) ? ':' . $parsed_url['pass'] : '';
+        $pass = ($user || $pass) ? "$pass@" : '';
+        $path = isset($parsed_url['path']) ? $parsed_url['path'] : '';
+        $query = isset($parsed_url['query']) ? '?' . $parsed_url['query'] : '';
+        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+        return "$scheme$user$pass$host$port$path$query$fragment";
+    }
+
+    public function isAllowed($url, $user_agent = "*")
+    {
         $purl = parse_url($url);
         unset($purl['scheme']);
         unset($purl['host']);
         unset($purl['port']);
         unset($purl['user']);
         unset($purl['pass']);
-        $url = $this->unparse_url($purl );
-        /*if (mb_substr($url, 0, 1, "UTF-8") !== '/') {
-            return true;
-        }*/
+        $url = $this->unparse_url($purl);
+        $user_agent = mb_strtolower($user_agent, "UTF-8");
+
         if ($this->current_user_agent !== null) {
             $user_agent = $this->current_user_agent;
         }
         $status = true;
-        if ( isset($this->rules[$user_agent])) {
-            foreach($this->rules[$user_agent] as $rl) {
-                $allowed = true;
-                foreach($rl['evalue'] as $part) {
-                    if ($part != '') {
-                        if (mb_substr($part, -1, 1, "UTF-8") == '$') {
-                            $part = rtrim($part, "$");
-                            $pos = stripos($url, $part);
-                            if ($pos === false || $pos + mb_strlen($part, "UTF-8") != mb_strlen($url, "UTF-8")) {
-                                $allowed = false;
-                                break;
-                            }
-                        } else {
-                            $pos = stripos($url, $part);
-                            if (mb_substr($part, 0, 1, "UTF-8") == '/') {
-                                if ($pos != 0 || $pos != false) {
-                                    $allowed = false;
-                                    break;    
-                                }
-                                
-                            }
-                            if ($pos === false ) {
-                                $allowed = false;
-                                break;
-                            }
-                        }
-                    } 
-                }
+        if (isset($this->rules[$user_agent])) {
+            foreach ($this->rules[$user_agent] as $rl) {
+                $allowed = (preg_match($rl['pattern'], $url) === 1);
                 if ($allowed === true) {
-                    if ( $rl['name'] == 'disallow') {
+                    if ($rl['name'] == 'disallow') {
                         $status = false;
                     } else {
                         $status = true;
                     }
                 }
-                
+
             }
         }
         return $status;
-   }
+    }
 }
--- test/test.php
+++ test/test.php
@@ -1,7 +1,7 @@
 <?php
-ini_set("display_errors",1);
+ini_set("display_errors", 1);
 error_reporting(E_ALL);
-require_once(__DIR__.'/../src/robotstxt.php');
+require_once(__DIR__ . '/../src/robotstxt.php');
 
 
 $robots = new Robotstxt();
@@ -16,19 +16,18 @@ Allow: /
 ");
 
 $task = array(
-			"/catalog/auto" => true,
-			"/ffaa/dd" => false,
-		);
-		
-foreach ($task as $url=>$t) {
-	if ($robots->isAllowed($url) === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
-}
+    "/catalog/auto" => true,
+    "/ffaa/dd" => false,
+);
 
+foreach ($task as $url => $t) {
+    if ($robots->isAllowed($url) === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
+}
 
 
 $robots->init("User-agent: *
@@ -38,20 +37,20 @@ Disallow: /cat
 ");
 
 $task = array(
-			"/" => true,
-			"/catalog" => true,
-			"/catalog/auto" => false,
-			"/auto" => true,
-			"/ru/cat" => true,
-		);
-		
-foreach ($task as $url=>$t) {
-	if ($robots->isAllowed($url) === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+    "/" => true,
+    "/catalog" => true,
+    "/catalog/auto" => false,
+    "/auto" => true,
+    "/ru/cat" => true,
+);
+
+foreach ($task as $url => $t) {
+    if ($robots->isAllowed($url) === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
 
@@ -90,93 +89,116 @@ $robots->init("
 	");
 
 $task = array(
-			'/blogs' => true,
-			'/ru/dlaaa' => false,
-			'/blogs/page.php?page=1&sort=price' => true,
-			'/home' => false,
-			'/car/m.php?page=1000&sort=price' => true,
-			'/m.php' => true,
-			'/m.ph' => false,
-			'/m.php?p=1' => false,
-			'/' => false,
-		);
+    '/blogs' => true,
+    '/ru/dlaaa' => false,
+    '/blogs/page.php?page=1&sort=price' => true,
+    '/home' => false,
+    '/car/m.php?page=1000&sort=price' => true,
+    '/m.php' => true,
+    '/m.ph' => false,
+    '/m.php?p=1' => false,
+    '/' => false,
+);
 
 $task_botd = array(
-			'/blogs' => true,
-		);
+    '/blogs' => true,
+);
 
 $task_bota = array(
-			'/blogs' => false,
-		);
+    '/blogs' => true,
+);
 
 $task_botcarbon = array(
-			'/carbon.php' => true,
-			'http://www.matras-market.ru/aksessuary/135x200/tempur?rand=dprice' => false,
-			'aksessuary/135x200/tempur?rand=dprice' => false,
-		);
+    '/carbon.php' => true,
+    'http://www.matras-market.ru/aksessuary/135x200/tempur?rand=dprice' => false,
+    'aksessuary/135x200/tempur?rand=dprice' => true,
+);
 
 
-foreach ($task as $url=>$t) {
-	if ($robots->isAllowed($url) === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+foreach ($task as $url => $t) {
+    if ($robots->isAllowed($url) === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
 
-
-foreach ($task_botd as $url=>$t) {
-	if ($robots->isAllowed($url, 'botd') === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+foreach ($task_botd as $url => $t) {
+    if ($robots->isAllowed($url, 'botd') === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
-foreach ($task_bota as $url=>$t) {
-	if ($robots->isAllowed($url, 'bota') === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+foreach ($task_bota as $url => $t) {
+    if ($robots->isAllowed($url, 'bota') === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
-foreach ($task_botcarbon as $url=>$t) {
-	if ($robots->isAllowed($url, 'botcarbon') === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+foreach ($task_botcarbon as $url => $t) {
+    if ($robots->isAllowed($url, 'botcarbon') === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
 $robots->init("User-agent: *
 Allow: *
 Disallow: /404.php
+Disallow: */8049776
 Host: faberlicsale.com");
 
 $task = array(
-			"http://faberlicsale.com/index.php" => true,
-			"http://faberlicsale.com/Voprosy-otvety.php" => true,
-			"http://faberlicsale.com/Consultant-Faberlic.php" => true,
-			"http://faberlicsale.com/News-Faberlic.php" => true,
-			"http://faberlicsale.com/O-Faberlic.php" => true,
-			"http://faberlicsale.com/Contacts.php" => true,
-		);
+    "https://stackoverflow.com/questions/8049776/xdebug-for-remote-server-not-connecting" => false,
+    "http://faberlicsale.com/index.php" => true,
+    "http://faberlicsale.com/Voprosy-otvety.php" => true,
+    "http://faberlicsale.com/Consultant-Faberlic.php" => true,
+    "http://faberlicsale.com/News-Faberlic.php" => true,
+    "http://faberlicsale.com/O-Faberlic.php" => true,
+    "http://faberlicsale.com/Contacts.php" => true,
+);
 
-foreach ($task as $url=>$t) {
-	if ($robots->isAllowed($url) === $t) {
-		echo("[TRUE] Доступ для '".$url."' определен верно\n");
-	} else {
-		echo("[FALSE] Доступ для '".$url."' определен неверно\n");
-		$count_error++;
-	}
+foreach ($task as $url => $t) {
+    if ($robots->isAllowed($url) === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
 }
 
-echo("Конец тестирования. Количество ошибок: ".$count_error."\n");
+$robots->init("# Исходный robots.txt:
+User-agent: Yandex
+Allow: /
+Allow: /catalog/auto
+Disallow: /catalog");
+
+$task = array(
+    "/catalog" => false,
+    "/catalog/moto" => false,
+    "/catalog/auto" => true,
+    "/catalog/auto/foto" => true,
+);
+
+foreach ($task as $url => $t) {
+    if ($robots->isAllowed($url, "yAnDeX") === $t) {
+        echo("[TRUE] Доступ для '" . $url . "' определен верно\n");
+    } else {
+        echo("[FALSE] Доступ для '" . $url . "' определен неверно\n");
+        $count_error++;
+    }
+}
+
+echo("Конец тестирования. Количество ошибок: " . $count_error . "\n");
 
 
